7f6ff41e3293028f5379940a3e784c5f8c94a542
Khanhbang280601 <khanhbang280601@gmail.com>
2023-08-01 16:50:07 +0700

    ex for hashset

---
+++ ReviewJava/src/main/java/org/example/setInterface/HashSet.java
@@ -0,0 +1,24 @@
+package org.example.setInterface;
+
+import java.util.Set;
+
+public class HashSet {
+    public Set<Integer> hashSet(){
+        Set<Integer> list = new java.util.HashSet<>();
+        System.out.println(list);
+
+        list.add(5);
+        list.add(1);
+        list.add(7);
+        list.add(3);
+        System.out.println(list);
+
+        list.clear();
+        list.add(3);
+        System.out.println(list);
+
+        list.remove(7);
+        System.out.println("Set after remove element in set: " + list);
+        return list;
+    }
+}
